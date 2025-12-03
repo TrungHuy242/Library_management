9eaa8df3712372e29625a6cab884ca24ad6f1087
Huy Trung <huytrung0102@gmail.com>
2025-12-03 19:16:15 +0700

    update sql

--- lib/library_db.sql
+++ lib/library_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2025 at 10:13 AM
+-- Generation Time: Dec 03, 2025 at 01:15 PM
 -- Server version: 10.6.15-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,8 +42,8 @@ CREATE TABLE `bandoc` (
 --
 
 INSERT INTO `bandoc` (`maBanDoc`, `hoTen`, `lop`, `sdt`, `email`, `ngaySinh`, `ngayDangKy`) VALUES
-(1, 'Nguyễn Văn An', 'D21CQCN01', '0905000111', 'an@student.com', '2003-05-15', '2025-11-29'),
-(2, 'Trần Thị Bình', 'D21CQCN02', '0905000222', 'binh@student.com', '2003-07-20', '2025-11-29'),
+(1, 'Nguyễn Văn An đã sửa', 'D21CQCN01', '0905000111', 'an@student.com', '2003-05-15', '2025-11-29'),
+(2, 'Trần Thị Bình', 'D21CQCN02', '0905000222', 'binh@mail.com', '2003-07-20', '2025-11-29'),
 (3, 'Nguyễn Ngọc Quyền', 'D20CQAT01', '0905000333', 'quyen@gmail.com', '2002-03-10', '2025-11-29'),
 (4, 'Lê Ngọc Tiến', 'D21CQCN03', '0905000444', 'tien@gmail.com', '2003-09-05', '2025-11-29'),
 (5, 'Đỗ Quốc Đạt', 'D21CQCN04', '0905000555', 'dat@gmail.com', '2003-01-12', '2025-11-29'),
@@ -80,7 +80,17 @@ INSERT INTO `ct_phieumuon` (`maPhieuMuon`, `maSach`, `soLuong`) VALUES
 (6, 7, 1),
 (7, 2, 1),
 (8, 8, 1),
-(10, 3, 1);
+(10, 3, 1),
+(13, 10, 1),
+(13, 11, 1),
+(14, 10, 1),
+(15, 6, 1),
+(16, 10, 1),
+(16, 11, 1),
+(17, 9, 1),
+(18, 10, 7),
+(20, 1, 1),
+(20, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -103,7 +113,13 @@ INSERT INTO `ct_phieutra` (`maPhieuTra`, `maSach`, `soLuong`, `tinhTrangSach`) V
 (1, 4, 1, 'Bình thường'),
 (1, 5, 1, 'Hỏng'),
 (2, 9, 1, 'Bình thường'),
-(2, 10, 1, 'Bình thường');
+(2, 10, 1, 'Bình thường'),
+(7, 6, 1, 'Bình thường'),
+(8, 1, 1, 'Bình thường'),
+(8, 2, 1, 'Bình thường'),
+(9, 10, 7, 'Bình thường'),
+(10, 10, 1, 'Bình thường'),
+(10, 11, 1, 'Bình thường');
 
 -- --------------------------------------------------------
 
@@ -127,7 +143,8 @@ CREATE TABLE `nhanvien` (
 INSERT INTO `nhanvien` (`maNV`, `hoTen`, `sdt`, `vaiTro`, `taiKhoan`, `matKhau`) VALUES
 (1, 'Nguyễn Văn Admin', '0909123456', 'Quản lý', 'admin', '123'),
 (2, 'Trương Thị Kim Ngân', '0909111222', 'Thủ thư', 'ngandu', 'ngudan'),
-(3, 'Từ Nguyễn Huyền Trang', '0909333444', 'Thủ thư', 'huyentrang', 'trang123');
+(3, 'Từ Nguyễn Huyền Trang', '0909333444', 'Thủ thư', 'huyentrang', 'huy123'),
+(4, 'Nguyễn Ngọc Quyền', '05646465165', 'Thủ thư', 'ngocquyen', 'ngocquyen123');
 
 -- --------------------------------------------------------
 
@@ -149,16 +166,23 @@ CREATE TABLE `phieumuon` (
 --
 
 INSERT INTO `phieumuon` (`maPhieuMuon`, `maBanDoc`, `maNV`, `ngayMuon`, `ngayHenTra`, `trangThai`) VALUES
-(1, 1, 1, '2025-11-20', '2025-12-04', 'Đang mượn'),
-(2, 2, 1, '2025-11-10', '2025-11-24', 'Quá hạn'),
+(1, 1, 1, '2025-11-20', '2025-12-04', 'Đã trả'),
+(2, 2, 1, '2025-11-10', '2025-11-24', 'Đã trả'),
 (3, 3, 2, '2025-10-25', '2025-11-08', 'Đã trả'),
 (4, 4, 1, '2025-10-15', '2025-11-05', 'Quá hạn'),
 (5, 5, 2, '2025-11-01', '2025-11-15', 'Đã trả'),
-(6, 6, 1, '2025-11-28', '2025-12-12', 'Đang mượn'),
-(7, 1, 2, '2025-11-15', '2025-11-29', 'Quá hạn'),
+(6, 6, 1, '2025-11-28', '2025-12-12', 'Đã trả'),
+(7, 1, 2, '2025-11-15', '2025-11-29', 'Đã trả'),
 (8, 7, 1, '2025-11-05', '2025-11-19', 'Quá hạn'),
-(9, 8, 2, '2025-11-25', '2025-12-09', 'Đang mượn'),
-(10, 3, 1, '2025-11-18', '2025-12-02', 'Đang mượn');
+(9, 8, 2, '2025-11-25', '2025-12-09', 'Đã trả'),
+(10, 3, 1, '2025-11-18', '2025-12-02', 'Đã trả'),
+(13, 2, 1, '2025-11-30', '2025-12-14', 'Đã trả'),
+(14, 8, 1, '2025-12-02', '2025-12-20', 'Đã trả'),
+(15, 3, 1, '2025-12-03', '2025-12-12', 'Đã trả'),
+(16, 1, 1, '2025-12-03', '2025-12-17', 'Đã trả'),
+(17, 4, 1, '2025-12-03', '2025-12-17', 'Đang mượn'),
+(18, 1, 1, '2025-12-03', '2025-12-17', 'Đã trả'),
+(20, 4, 1, '2025-12-03', '2025-12-17', 'Đã trả');
 
 -- --------------------------------------------------------
 
@@ -181,7 +205,11 @@ CREATE TABLE `phieutra` (
 
 INSERT INTO `phieutra` (`maPhieuTra`, `maPhieuMuon`, `maNV`, `ngayTra`, `tienPhat`, `ghiChu`) VALUES
 (1, 3, 2, '2025-11-20', 50000, 'Quá hạn 12 ngày'),
-(2, 5, 1, '2025-11-25', 30000, 'Quá hạn 10 ngày');
+(2, 5, 1, '2025-11-25', 30000, 'Quá hạn 10 ngày'),
+(7, 15, 1, '2025-12-03', 0, ''),
+(8, 20, 1, '2025-12-03', 0, ''),
+(9, 18, 1, '2025-12-03', 0, ''),
+(10, 16, 4, '2025-12-03', 0, '');
 
 -- --------------------------------------------------------
 
@@ -206,17 +234,17 @@ CREATE TABLE `sach` (
 --
 
 INSERT INTO `sach` (`maSach`, `tenSach`, `maTacGia`, `maTheLoai`, `namXuatBan`, `nhaXuatBan`, `soLuongTong`, `soLuongHienTai`, `trangThai`) VALUES
-(1, 'Lập Trình Java Cơ Bản', 2, 1, '2023', 'NXB Giáo Dục', 20, 15, 'Bình thường'),
-(2, 'Java Nâng Cao', 4, 1, '2022', 'NXB Kim Đồng', 15, 10, 'Bình thường'),
-(3, 'Cấu Trúc Dữ Liệu & Giải Thuật', 3, 1, '2021', 'NXB Khoa Học', 25, 18, 'Bình thường'),
+(1, 'Lập Trình Java Cơ Bản', 2, 1, '2023', 'NXB Giáo Dục', 20, 16, 'Bình thường'),
+(2, 'Java Nâng Cao', 4, 1, '2022', 'NXB Kim Đồng', 15, 13, 'Bình thường'),
+(3, 'Cấu Trúc Dữ Liệu & Giải Thuật', 3, 1, '2021', 'NXB Khoa Học', 25, 20, 'Bình thường'),
 (4, 'Cơ Sở Dữ Liệu', 3, 2, '2020', 'NXB Đại Học Quốc Gia', 30, 25, 'Bình thường'),
 (5, 'Toán Rời Rạc', 3, 3, '2024', 'NXB Giáo Dục', 40, 35, 'Bình thường'),
-(6, 'Mắt Biếc', 5, 4, '2019', 'NXB Trẻ', 50, 45, 'Bình thường'),
-(7, 'Dế Mèn Phiêu Lưu Ký', 6, 4, '1941', 'NXB Văn Học', 60, 58, 'Bình thường'),
+(6, 'Mắt Biếc', 5, 4, '2019', 'NXB Trẻ', 50, 46, 'Bình thường'),
+(7, 'Dế Mèn Phiêu Lưu Ký', 6, 4, '1941', 'NXB Văn Học', 60, 59, 'Bình thường'),
 (8, 'Chí Phèo', 7, 4, '1941', 'NXB Văn Học', 35, 35, 'Bình thường'),
-(9, 'C++ Programming', 1, 1, '2018', 'NXB Giáo Dục', 18, 12, 'Bình thường'),
+(9, 'C++ Programming', 1, 1, '2018', 'NXB Giáo Dục', 18, 11, 'Bình thường'),
 (10, 'Lập Trình Hướng Đối Tượng', 4, 1, '2023', 'NXB Kim Đồng', 22, 8, 'Bình thường'),
-(11, 'Trung hẹ hẹ', 5, 3, '2025', 'Hoainhi', 12, 12, 'Bình thường');
+(11, 'Trung hẹ hẹ', 5, 3, '2025', 'hoanhi', 12, 12, 'Bình thường');
 
 -- --------------------------------------------------------
 
@@ -343,43 +371,43 @@ ALTER TABLE `theloai`
 -- AUTO_INCREMENT for table `bandoc`
 --
 ALTER TABLE `bandoc`
-  MODIFY `maBanDoc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `maBanDoc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `nhanvien`
 --
 ALTER TABLE `nhanvien`
-  MODIFY `maNV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `maNV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `phieumuon`
 --
 ALTER TABLE `phieumuon`
-  MODIFY `maPhieuMuon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `maPhieuMuon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `phieutra`
 --
 ALTER TABLE `phieutra`
-  MODIFY `maPhieuTra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `maPhieuTra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `sach`
 --
 ALTER TABLE `sach`
-  MODIFY `maSach` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `maSach` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tacgia`
 --
 ALTER TABLE `tacgia`
-  MODIFY `maTacGia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `maTacGia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `theloai`
 --
 ALTER TABLE `theloai`
-  MODIFY `maTheLoai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `maTheLoai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
